795fdaeca980696b46889bf9ca268d8ffae75922
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-09-24 20:44:20 -0400

    basic username authentication, console.log of all posts, and some dom manipulation when a correct username/password were entered

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,15 +1,55 @@
 document.addEventListener('DOMContentLoaded', function(){
     let loginButton = document.querySelector('#login')
-    let usernameInput = document.querySelector('#username')
+    let emailInput = document.querySelector('#username')
     let passwordInput = document.querySelector('#password')
-    
-    loginButton.addEventListener('click', function(){
-        let username = usernameInput.value
-        let password = passwordInput.value
-        console.log(username)
-        console.log(password)
-        //document.querySelector("#form").innerHTML = " "
+
+    fetch("http://thesi.generalassemb.ly:8080/post/list", {
+        method: 'GET'
+    })
+    .then((response )=> {
+        return response.json();
+    })
+    .then((json) =>{
+        console.log(json)
     })
     
-    
+    loginButton.addEventListener('click', function(){
+        let email = emailInput.value
+        let password = passwordInput.value
+        console.log(email)
+        console.log(password)
+        
+
+        fetch("http://thesi.generalassemb.ly:8080/login", {
+            method: 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+            .then((response )=> {
+                return response.json();
+            })
+            .then((json) =>{
+                
+                 document.querySelector("#form").innerHTML = " "
+                let index = email.indexOf('@')
+                let profileName = email.slice(0, index)
+                let linkToProfile = document.createElement('a')
+                linkToProfile.innerHTML = profileName
+                linkToProfile.href = "#"
+                document.querySelector("#header").appendChild(linkToProfile)
+                console.log(linkToProfile)
+
+                //console.log(json)
+            })
+            .catch(function(error){
+                alert(error)
+            })
+    })
+      
 })
